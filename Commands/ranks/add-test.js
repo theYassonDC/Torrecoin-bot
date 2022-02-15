f65c77ba438403ab4a2eb557321fded54b84ac7f
Juanda <98669750+theYassonDC@users.noreply.github.com>
2022-02-15 04:19:53 +0000

    updates commands rank test

--- Commands/ranks/add-test.js
+++ Commands/ranks/add-test.js
@@ -5,6 +5,7 @@ module.exports = {
    name: "add-tester",
   alias: ["add-testiador", "give-tester"],
   async execute(client, message, args){
+    const rol = message.guild.roles.cache.find(role => role.id === `915770314326179880`)
     const administrador = await adm.findOne({user: message.author.id})
     if(!administrador) return message.reply("<:modBag:915764671204692008> | **No eres administrador para usar este comando**")  
   const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
@@ -15,7 +16,8 @@ module.exports = {
               user: member.id
           })
           await datosnuevos.save()
-          return message.reply(`Se agrego a la lista de testiadores el usuario ${member}!`)
+          await message.guild.members.cache.get(member.id).roles.add(rol)
       }
+    message.reply(`Se agrego a la lista de testiadores el usuario ${member}!`)
   }
 }
--- Commands/ranks/del-tester.js
+++ Commands/ranks/del-tester.js
@@ -6,14 +6,15 @@ module.exports = {
   async execute(client, message, args){
     const administrador = await adm.findOne({user: message.author.id})
     if(!administrador) return message.reply("<:modBag:915764671204692008> | **No eres administrador para usar este comando**") 
+    const rol = message.guild.roles.cache.find(role => role.id === `915770314326179880`)
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])  
     const data = await tester.findOne({user: member.id})
     if(!data) return message.channel.send("No se encuentra este usuario en lista.")
     if(!data.user) return message.channel.send(`El usuario ${member} no es tester`)
     if(data){
       await data.delete()
-      return message.channel.send(`Se removio al usuario ${member} de testiadores de Torrecoin`)
+      await message.guild.members.cache.get(member.id).roles.remove(rol)
     }
-    
+    message.channel.send(`Se removio al usuario ${member} de testiadores de Torrecoin`)
   }
 }
