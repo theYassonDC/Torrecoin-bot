dd1ce85b6ea5512bff67ad6f3cd2e73191ee75ef
Juanda <98669750+theYassonDC@users.noreply.github.com>
2022-04-14 08:37:34 +0000

    minium updates status bot

--- events/guils/botstatus.js
+++ events/guils/botstatus.js
@@ -2,10 +2,10 @@ module.exports = {
   name: "ready",
   async run(client){
   const activities = ["Developing..", "Listo para la gran beta", "Updates commands bugs", "En desarrollo | $help", "Economy bot"]
-
     setInterval(()=>{
       let status = activities[Math.floor(Math.random() * activities.length)]
       client.user.setPresence({activities: [{ name: `${status}` }]})
     }, 5030)
   }
+  
 }
